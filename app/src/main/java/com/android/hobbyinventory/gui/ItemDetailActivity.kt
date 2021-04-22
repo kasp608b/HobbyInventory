1de517658b7cf2bad85dc50cc6a1824aecaab0b5
Lumby98 <lumby98@gmail.com>
2021-04-22 16:17:34 +0200

    small thing

--- app/src/main/java/com/android/hobbyinventory/gui/ItemDetailActivity.kt
+++ app/src/main/java/com/android/hobbyinventory/gui/ItemDetailActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import com.android.hobbyinventory.R
 
 class ItemDetailActivity : AppCompatActivity() {
+
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_detail)
+
+        if(intent.extras != null) {
+            val extras: Bundle = intent.extras!!
+        }
 
 
     }
