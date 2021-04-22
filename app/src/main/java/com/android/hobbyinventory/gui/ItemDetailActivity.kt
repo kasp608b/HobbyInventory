41ea1da05eb472ddd7f8d67d927e40acc495b627
Lumby98 <lumby98@gmail.com>
2021-04-22 16:07:42 +0200

    did the visuals for the item details

---
+++ app/src/main/java/com/android/hobbyinventory/gui/ItemDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.android.hobbyinventory.gui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.hobbyinventory.R
+
+class ItemDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_detail)
+
+
+    }
+}
