d77720039aa56ee0f2dab40e1ae30ab81c5d7789
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-04-22 17:28:48 +0200

    request codes

--- app/src/main/java/com/android/hobbyinventory/gui/ItemDetailActivity.kt
+++ app/src/main/java/com/android/hobbyinventory/gui/ItemDetailActivity.kt
@@ -6,7 +6,10 @@ import com.android.hobbyinventory.R
 
 class ItemDetailActivity : AppCompatActivity() {
 
-    
+    private val PERMISSION_REQUEST_CODE = 1
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_FILE = 101
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_BITMAP = 102
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_detail)
