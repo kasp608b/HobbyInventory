a689e7c5739799ceaeb43600311d75c5f9acbcc4
scupak <anto1095@easv365.dk>
2021-04-22 18:37:36 +0200

    stuff

--- app/src/main/java/com/android/hobbyinventory/gui/CollectionDetailActivity.kt
+++ app/src/main/java/com/android/hobbyinventory/gui/CollectionDetailActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.hobbyinventory.R
 
-class CollectionActivity : AppCompatActivity() {
+class CollectionDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_collection)
+        setContentView(R.layout.activity_collection_detail)
     }
 }
