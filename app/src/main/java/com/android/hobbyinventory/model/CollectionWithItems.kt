3a66351618de64983a94c1ada959d9b08cb19741
zanaxdk <31621017+zanaxdk@users.noreply.github.com>
2021-05-05 15:40:09 +0200

    Update CollectionWithItems.kt

--- app/src/main/java/com/android/hobbyinventory/model/CollectionWithItems.kt
+++ app/src/main/java/com/android/hobbyinventory/model/CollectionWithItems.kt
@@ -3,6 +3,9 @@ package com.android.hobbyinventory.model
 import androidx.room.Embedded
 import androidx.room.Relation
 
+/**
+ *This class defines the helper entity used to join the collections with their items
+ */
 data class CollectionWithItems(
     @Embedded val collection: BECollection,
     @Relation(
